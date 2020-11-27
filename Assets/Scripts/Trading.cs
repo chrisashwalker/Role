0456e242096d1f67d23b88f4f8609f621be8410a
chrisashwalker <hello@chrisashwalker.com>
2020-11-27 17:49:38 +0000

    Update

--- Assets/Scripts/Trading.cs
+++ Assets/Scripts/Trading.cs
@@ -1,18 +1,28 @@
 using System.Collections.Generic;
 
 public static class Trading{
-    public static List<Item> FindSaleItems(Character character){
-        List<Item> saleItems = new List<Item>();
-        foreach (Item item in character.Storage.StoredItems){
-            saleItems.Add(item);
+    public static bool InTrade{get;set;} = false;
+    public static Character Buyer{get;set;}
+    public static Character Seller{get;set;}
+    public static List<Item> SaleItems{get;set;}
+    public static void FindSaleItems(Character buyer, Character seller){
+        Buyer = buyer;
+        Seller = seller;
+        SaleItems = new List<Item>();
+        foreach (Item item in Seller.Storage.StoredItems){
+            SaleItems.Add(item);
         }
-        return saleItems;
     }
 
-    public static void SellItem(Character sellingCharacter, Character buyingCharacter, Item item){
-        sellingCharacter.Storage.StoredItems.Remove(item);
-        sellingCharacter.Coins += item.Value;
-        buyingCharacter.Storage.StoredItems.Add(item);
-        buyingCharacter.Coins += item.Value;
+    public static void TradeItem(Item item){
+        if (Buyer.Coins >= item.Value && Buyer.Storage.StoredItems.Count < Buyer.Storage.MaxCapacity){
+            Buyer.Coins -= item.Value;
+            Seller.Coins += item.Value;
+            Seller.Storage.StoredItems.Remove(item);
+            Buyer.Storage.StoredItems.Add(item);
+            InTrade = false;
+        } else {
+            InTrade = true;
+        }
     }
 }
